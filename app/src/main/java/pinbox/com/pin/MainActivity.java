ea5f0f9f1369bc4fa85e44ee9ad871a21736880d
ณัฐพากย์ จักษุอินทร์ <015240@rama.mahidol.ac.th>
2016-01-14 11:09:13 +0700

    HttpUrlConnection

--- app/src/main/java/pinbox/com/pin/MainActivity.java
+++ app/src/main/java/pinbox/com/pin/MainActivity.java
@@ -1,8 +1,10 @@
 package pinbox.com.pin;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -12,13 +14,53 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+    private URL url;
+    private HttpURLConnection urlConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        try {
+
+            url = new URL("http://localhost:8080/find/farm");
+            Log.e("check", "1");
+            urlConnection = (HttpURLConnection)url.openConnection();
+            Log.e("check", "2");
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            Log.e("check", "3");
+            InputStream in = urlConnection.getInputStream();
+            Log.e("check", "4");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = reader.readLine())!= null){
+                buffer.append(line);
+            }
+            Log.e("check", buffer.toString());
+
+        }catch (Exception e){
+
+        }finally {
+            urlConnection.disconnect();
+
+        }
 
 
         FacebookSdk.sdkInitialize(getApplicationContext());
@@ -45,11 +87,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class HttpRestClient extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+
+
+            return null;
+        }
     }
 }
